972e98ce993a2b5396912cc09a753c74bdb7c12a
Gildo Komba <161140108+gkomba@users.noreply.github.com>
2025-06-26 02:01:09 +0100

    Update route.ts dataSistema

--- app/api/system-data/route.ts
+++ app/api/system-data/route.ts
@@ -4,7 +4,7 @@ export async function GET(req: NextRequest) {
   try {
     const FIREBASE_BASE_URL = 'https://esp-api-10fa5-default-rtdb.firebaseio.com'
     
-    const [circuitResponse, ledResponse] = await Promise.all([
+    const [circuitResponse, ledResponse, sistemaResponse] = await Promise.all([
       fetch(`${FIREBASE_BASE_URL}/circuito.json`, {
         headers: {
           'Accept': 'application/json',
@@ -16,19 +16,26 @@ export async function GET(req: NextRequest) {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         }
+      }),
+      fetch(`${FIREBASE_BASE_URL}/sistema.json`, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
       })
     ])
 
-    if (!circuitResponse.ok || !ledResponse.ok) {
-      throw new Error(`HTTP Error: ${circuitResponse.status} / ${ledResponse.status}`)
+    if (!circuitResponse.ok || !ledResponse.ok || !sistemaResponse) {
+      throw new Error(`HTTP Error: ${circuitResponse.status} / ${ledResponse.status} / ${sistemaResponse}`)
     }
 
     const circuitData = await circuitResponse.json()
     const ledData = await ledResponse.json()
+    const sistemaData = await sistemaResponse.json()
+    const allData = circuitData + ledData + sistemaData
 
     return Response.json({
-      circuito: circuitData,
-      led: ledData
+      allData: allData
     })
   } catch (error) {
     console.error('Erro ao buscar dados do sistema:', error)
